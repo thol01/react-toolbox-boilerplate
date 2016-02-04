f2656703eb92d191f20487f9dd51859f01b4757b
Thomas Olsson <thomas@Thomass-MacBook-Pro.local>
2016-02-04 14:31:41 +0100

    The application now works. It maybe that some node modules can be removed.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import ToolboxApp from 'react-toolbox/lib/app';
+import Button from 'react-toolbox/lib/button';
 
 export default class App extends Component {
-  render() {
-    return (
-      <h1>React Toolbox!</h1>
-    );
-  }
+    render() {
+        return (
+            <ToolboxApp>
+                <h1>React Toolbox!</h1>
+                <Button raised accent label='test'/>
+            </ToolboxApp>
+        );
+    }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,59 @@
 const path = require('path');
 const webpack = require('webpack');
+const autoprefixer = require('autoprefixer');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 
 module.exports = {
-  devtool: 'eval',
-  entry:  __dirname + "/src/index.js",
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/static/'
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin()
-  ],
-  module: {
-    loaders: [{
-      test: /\.js$/,
-      loaders: ['react-hot', 'babel'],
-      include: path.join(__dirname, 'src')
-    }]
-  },
+    devtool: 'inline-source-map',
+    entry: __dirname + "/src/index.js",
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/static/'
+    },
+    resolve: {
+        extensions: ['', '.jsx', '.scss', '.js', '.json'],
+        modulesDirectories: [
+            'node_modules',
+            path.resolve(__dirname, './node_modules')
+        ]
+    },
 
-  devServer: {
-    contentBase: "./src/public",
-    colors: true,
-    historyApiFallback: true,
-    inline: true
-  }
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loaders: ['react-hot', 'babel'],
+                include: path.join(__dirname, 'src')
+            },
+            {
+                test: /(\.scss|\.css)$/,
+                loader: ExtractTextPlugin.extract('style', 'css?sourceMap&modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss!sass?sourceMap!toolbox')
+            }
+        ]
+    },
+
+    toolbox: {
+        theme: path.join(__dirname, 'src/public/toolbox-theme.scss')
+    },
+
+    postcss: [autoprefixer],
+
+    plugins: [
+        new ExtractTextPlugin('react-toolbox.css', { allChunks: true }),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin(),
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify('development')
+        })
+    ],
+
+    devServer: {
+        contentBase: "./src/public",
+        colors: true,
+        historyApiFallback: true,
+        inline: true
+    }
 };
 
